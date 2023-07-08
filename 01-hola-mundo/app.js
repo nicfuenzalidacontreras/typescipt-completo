fbd465d85ba353e2e29438c392806f251b6eeada
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-07-08 16:32:08 -0400

    hola mundo y tsconfig

---
+++ 01-hola-mundo/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const msg = 'Hola Mundo';
+const heroe = {
+    name: "Batman",
+    age: 46
+};
+heroe.age = 50;
+console.log(heroe.age + 1);
---
+++ 01-hola-mundo/app.ts
@@ -0,0 +1,9 @@
+const msg: string = 'Hola Mundo';
+
+const heroe = {
+    name: "Batman",
+    age: 46
+}
+
+heroe.age = 50
+console.log(heroe.age + 1);
