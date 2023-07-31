357078890369dfc2f7b6ee00a226e492880e2511
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-07-31 18:02:24 -0400

    caracteristicas de es6 o javascript2015

---
+++ bases/es6/let-var.ts
@@ -0,0 +1,12 @@
+(() => {
+    // console.log(a);
+
+    const nombre: string = 'Nicolás'
+
+    const getName = (): void => {
+        console.log('viejo getName');
+    }
+
+    // getName = () => { console.log('Nuevo getName') };
+    // getName()
+})()
