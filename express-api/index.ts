0f21c2254ddc2fb0053ef136ac304010a313a226
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-08-02 15:17:45 -0400

    usando librerias que no están escritas en typescript

---
+++ express-api/index.ts
@@ -0,0 +1,15 @@
+//const express = require('express')
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.status(401).json({
+    ok: false,
+    msg: 'No hay token!'
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
